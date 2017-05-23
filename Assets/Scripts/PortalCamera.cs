87e2ce3278b5b224946f1e3c8aa07639fe526745
KillianMcCabe <killianparker93@gmail.com>
2017-05-23 22:29:38 +0100

    Portals now work (visually) if rotations don't match

--- Assets/Scripts/PortalCamera.cs
+++ Assets/Scripts/PortalCamera.cs
@@ -18,16 +18,20 @@ public class PortalCamera : MonoBehaviour {
 		Vector3 portalPos = portal.transform.position;
 		Vector3 otherPortalPos = otherPortal.transform.position;
 		Vector3 playerCameraPos = playerCamera.transform.position;
+        
+		float anglarDifferenceBetweenPortalRotations = Quaternion.Angle(portal.transform.rotation, otherPortal.transform.rotation);
+		Quaternion portalRotationalDifference = Quaternion.AngleAxis(anglarDifferenceBetweenPortalRotations, Vector3.up);
+
+        //Quaternion relative = Quaternion.Inverse(portal.transform.rotation) * otherPortal.transform.rotation; // get relative difference between two rotations
+        Quaternion relative = Quaternion.Inverse(otherPortal.transform.rotation) * portal.transform.rotation; // get relative difference between two rotations
 
         // adjust position of camera
-		Vector3 playerOffsetFromPortal = playerCameraPos - otherPortalPos;
+        Vector3 playerOffsetFromPortal = playerCameraPos - otherPortalPos;
+        playerOffsetFromPortal = relative * playerOffsetFromPortal;
         transform.position = portalPos + playerOffsetFromPortal;
 
         // adjust rotation of camera
-		float anglarDifferenceBetweenPortalRotations = Quaternion.Angle(portal.transform.rotation, otherPortal.transform.rotation);
-
-		Quaternion portalRotationalDifference = Quaternion.AngleAxis(anglarDifferenceBetweenPortalRotations, Vector3.up);
-		Vector3 newFacingDirection = portalRotationalDifference * playerCamera.transform.forward;
-		transform.rotation = Quaternion.LookRotation(newFacingDirection, Vector3.up);
+        Vector3 newFacingDirection = relative * playerCamera.transform.forward;
+        transform.rotation = Quaternion.LookRotation(newFacingDirection, Vector3.up);
     }
 }
