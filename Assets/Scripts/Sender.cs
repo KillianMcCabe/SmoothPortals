bf9e3fe049564f820f24303644dd8451280f5487
KillianMcCabe <killianparker93@gmail.com>
2017-12-10 17:57:28 +0000

    previous commit missed a file

--- Assets/Scripts/Sender.cs
+++ Assets/Scripts/Sender.cs
@@ -23,28 +23,11 @@ public class Sender : MonoBehaviour {
         }
     }
 
-    // transport player to the equivalent position in the other portal
-	private void teleport()
-    {
-        // get relative difference between two rotations (i.e. the quaternion that would turn this rotation into that rotation)
-        // so say we want quaternion d which we can use like so: d * q1 = q2
-        // knowing how we want to use it, we can thus caluclate it as: d = q2 * inverse(q1)
-        Quaternion relativeDiff = receiver.transform.rotation * Quaternion.Inverse(transform.rotation);
-        relativeDiff *= Quaternion.Euler(0, 180, 0);
-        
-        Vector3 positionOffset = currentlyOverlappingObject.transform.position - transform.position;
-        positionOffset = relativeDiff * positionOffset;
-
-        // rotate
-        currentlyOverlappingObject.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().Rotate(relativeDiff);
-
-        // position
-        currentlyOverlappingObject.transform.position = receiver.transform.position + positionOffset;
-    }
-
 
     void OnTriggerEnter(Collider other)
     {
+        // This will break if two portable objects are passing through the same portal at the same time
+        // TODO: Reimplement in Portable script
         if (other.GetComponentInParent<Portable>() != null)
         {
             currentlyOverlappingObject = other.GetComponentInParent<Portable>();
@@ -57,6 +40,10 @@ public class Sender : MonoBehaviour {
     void OnTriggerExit(Collider other)
     {
         if (other.GetComponentInParent<Portable>() != null)
+        {
+            currentlyOverlappingObject = null;
+        }
+        else if (other.GetComponent<Portable>() != null)
         {
             currentlyOverlappingObject = null;
         }
